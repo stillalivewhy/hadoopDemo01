44a12cc5d6c8bfd8b40c2b0a4e163d12b8289776
xiangmingxiong <xyt1617@163.com>
2021-07-12 11:54:29 +0800

    hadoopDemo02

---
+++ src/main/java/com/xmx/hadoop/mapreduce/demo03/FlowMapper.java
@@ -0,0 +1,25 @@
+package com.xmx.hadoop.mapreduce.demo03;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class FlowMapper extends Mapper<LongWritable, Text, Text, FlowBean> {
+
+    Text k = new Text();
+
+    FlowBean v = new FlowBean();
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String str = value.toString();
+        String[] values = str.split("\t");
+        k.set(values[1]);
+        v.setUpFlow(Long.parseLong(values[values.length - 3]));
+        v.setDownFlow(Long.parseLong(values[values.length - 2]));
+        v.setSumFlow();
+        context.write(k, v);
+    }
+}
---
+++ src/main/java/com/xmx/hadoop/mapreduce/demo03/FlowReducer.java
@@ -0,0 +1,28 @@
+package com.xmx.hadoop.mapreduce.demo03;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class FlowReducer extends Reducer<Text, FlowBean, Text, FlowBean> {
+
+    private FlowBean v = new FlowBean();
+
+    @Override
+    protected void reduce(Text key, Iterable<FlowBean> values, Context context) throws IOException, InterruptedException {
+
+        Long upFlow = 0l;
+        Long downFlow = 0l;
+
+        for (FlowBean value : values) {
+            upFlow += value.getUpFlow();
+            downFlow += value.getDownFlow();
+        }
+
+        v.setUpFlow(upFlow);
+        v.setDownFlow(downFlow);
+        v.setSumFlow();
+        context.write(key, v);
+    }
+}
